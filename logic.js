1e95532240310fc70a4cfc291ca524310ac09663
ericmagallan <ericmagallan85@gmail.com>
2019-02-19 00:28:08 -0800

    Updated logic and jquery to logic.js

--- logic.js
+++ logic.js
@@ -14,16 +14,34 @@
 // When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
-
+// alert("Testing");
 
 //Global Variables
 
 //Crystal Variables
 
-var amethyst = 0;
-var aquamarine = 0;
-var chalcanthite = 0;
-var flourite = 0;
+var crystal = {
+    amethyst:
+    {
+        name: "amethyst",
+        value: 0
+    },
+    fluorite: 
+    {
+        name: "flourite",
+        value: 0
+    },
+    aquamarine:
+    {
+        name: "aquamarine",
+        value: 0
+    },
+    chalanthite:
+    {
+        name: "chalanthite",
+        value: 0
+    },
+}
 
 //Scores (Current and Target)
 var currentScore = 0;
@@ -34,5 +52,23 @@ var wins = 0;
 var losses = 0;
 
 //Functions
+var startGame = function() {
+
+};
 
 //Main Process
+$("#amethyst").click(function() {
+    alert("test");
+});
+
+$("#fluorite").click(function() {
+    alert("test");
+});
+
+$("#aquamarine").click(function() {
+   alert("test"); 
+});
+
+$("#chalanthite").click(function() {
+    alert("test");
+});

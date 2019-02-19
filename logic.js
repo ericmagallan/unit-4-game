f7521eda046d12d85ede5ddc98c32377eb8ea6f3
ericmagallan <ericmagallan85@gmail.com>
2019-02-18 21:15:22 -0800

    Updated the html and javascript structure. No game functionality just yet.

--- logic.js
+++ logic.js
@@ -1,1 +1,38 @@
-alert("Testing this alert feature");
+// This is where my javascript will live. It will be the where all the magic happens in terms of how the game functions and performs. It will manipulate the index.html page using DOM manipulation.
+
+
+//Here's how the app works:
+
+// There will be four crystals displayed as buttons on the page.
+// The player will be shown a random number at the start of the game.
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+// Your game will hide this amount until the player clicks a crystal.
+// When they do click one, update the player's score counter.
+// The player wins if their total score matches the random number from the beginning of the game.
+// The player loses if their score goes above the random number.
+// The game restarts whenever the player wins or loses.
+// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+
+
+//Global Variables
+
+//Crystal Variables
+
+var amethyst = 0;
+var aquamarine = 0;
+var chalcanthite = 0;
+var flourite = 0;
+
+//Scores (Current and Target)
+var currentScore = 0;
+var targetScore = 0;
+
+//Wins and Losses
+var wins = 0;
+var losses = 0;
+
+//Functions
+
+//Main Process
